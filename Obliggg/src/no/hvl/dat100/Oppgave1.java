c75edd232e2d0f389f1164b0a08d05dca41ebd8d
ramlawarsame <ramlawarsame@localhost>
2021-09-10 15:02:41 +0200

    Innlevering 1

---
+++ Obliggg/src/no/hvl/dat100/Oppgave1.java
@@ -0,0 +1,14 @@
+package no.hvl.dat100;
+
+public class Oppgave1 {
+	public static void main(String[] args) {
+		int n = 10; 
+		// vi kan bytte på verdien for å se om programet gir korrekt utskrift.
+		// programet kjøres ikke ved negative verdi.
+		for(int i = 1; i < n; i++ ) 
+		{
+			System.out.println(i); 
+		}
+		 
+	}
+}
